f2e654ae44e21f6dfb3af4b15d738aec364da0e7
raoneinet <raoneinet@hotmail.com>
2025-05-03 18:02:38 +0100

    adding provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,20 @@ import {Stacks} from "@/components/stacks"
 import {Projects} from "@/components/projects"
 import {Footer} from "@/components/footer"
 import {Promotion} from "@/components/promotion"
+import {ThemeProvider} from "@/contexts/themeContext"
 
 
 const Page = ()=>{
   return (
-    <div className="mx-auto">
-      <Header/>
-      <Stacks/>
-      <Projects/>
-      <Promotion/>
-      <Footer/>
-    </div>
+    <ThemeProvider>
+      <div className="mx-auto">
+        <Header/>
+        <Stacks/>
+        <Projects/>
+        <Promotion/>
+        <Footer/>
+      </div>
+    </ThemeProvider>
   )
 }
 
