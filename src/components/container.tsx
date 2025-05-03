464baf3809fd9a9809e7f81ad21c8ac2adc4ff35
raoneinet <raoneinet@hotmail.com>
2025-05-03 18:18:00 +0100

    add darkmode icon and spreading context into components

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -3,10 +3,17 @@ import {Stacks} from "@/components/stacks"
 import {Projects} from "@/components/projects"
 import {Footer} from "@/components/footer"
 import {Promotion} from "@/components/promotion"
+import {ThemeContext} from "@/contexts/themeContext"
+import {useContext} from "react"
 
 export const Container = ()=>{
+
+    const themeCtx = useContext(ThemeContext)
+
     return (
-        <div className="mx-auto">
+        <div className={`mx-auto
+            ${themeCtx?.theme === "dark" ? "bg-black text-white" : "bg-white text-black"}
+        `}>
             <Header/>
             <Stacks/>
             <Projects/>
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -3,7 +3,11 @@
 export const Menu = ()=>{
     return (
         <div className="">
-            <nav>
+            <nav className="flex gap-2 items-center">
+                <img 
+                    src="/icons/day-and-night.png" 
+                    className="w-5 h-5 transparent"
+                    onClick={()=>{}}/>
                 <ul className="flex gap-3 list-none text-lg text-gray-300">
                     <a href="#banner" className="hover:text-gray-400 hover:scale-110"><li>Home</li></a>
                     <a href="#project-session" className="hover:text-gray-400 hover:scale-110"><li>Portfolio</li></a>
