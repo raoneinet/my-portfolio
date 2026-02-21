5171bc30604bc04c46dc73aca5d7955b86a995d6
raoneinet <raoneinet@hotmail.com>
2026-02-21 00:11:45 +0000

    quick fix

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -9,7 +9,6 @@ export const Menu = ()=>{
                     <Link href="#banner" className="hover:text-[#6ef0c8]"><li>Home</li></Link>
                     <Link href="#whoami" className="hover:text-[#6ef0c8]"><li>About me</li></Link>
                     <Link href="#projects" className="hover:text-[#6ef0c8]"><li>Portfolio</li></Link>
-                    <Link href="#contact" className="hover:text-[#6ef0c8]"><li>Contact</li></Link>
                 </ul>
             </nav>
         </div>
